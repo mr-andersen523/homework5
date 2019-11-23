da127adf02ad9393ebdef0583280ff68bc7dfcba
mr andersen <chasea523@gmail.com>
2019-11-23 12:12:12 -0800

    setInterval function and save butn local storage

--- script.js
+++ script.js
@@ -39,5 +39,28 @@ $( document ).ready(function() {
         hours = hours ? hours : 12;
         return hours + ampm;
     }
-    amPm();
-    
+amPm();
+
+// function to change event block color for past present and future
+function updateColors(){
+    var currentTime = new Date().getHours();
+    for (var i = 9; i < 18; i++) { 
+     if ($(`#${i}`).data("time") == currentTime){
+        $(`#text${i}`).addClass( "present");
+    } else if (currentTime < $(`#${i}`).data("time")) {
+        $(`#text${i}`).addClass( "future");
+    }
+}
+}
+
+setInterval(function() {
+    updateColors();
+}, 1000);
+
+var saveBtn = $('.saveBtn');
+saveBtn.on('click', function(){
+    let eventId = $(this).attr('id');
+    let eventText = $(this).parent().siblings().children('.event').val();
+    localStorage.setItem(eventId, eventText);
+});
+});
